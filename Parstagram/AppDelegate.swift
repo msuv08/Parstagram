8cda7cdecba64d234795d8ea61d97861f260c3fb
Mihir Suvarna <49384703+msuv08@users.noreply.github.com>
2021-08-07 00:19:07 -0500

    Created Parstagram, added sign-in and login features

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "p6lMpQBIxmIDPWUFu6NahPnjBT9Bm2z78rGHNqYE"
+                $0.clientKey = "BElEktinIbS8AD3pNmmlkJIWoWhgOS0UcbuVfKJe"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
